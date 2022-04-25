a5d14651a7824e8366a0b51b9e6ce5e4003a49d7
Luis Jean <luisjeanknowsbest@gmail.com>
2022-04-24 23:13:40 -0400

    First commit before wrapping up

--- sudoku.py
+++ sudoku.py
@@ -21,3 +21,14 @@ defaultgrid = [
 
 font = pygame.font.SysFont("comicsans", 40)
 font1 = pygame.fontSysFont("comicsans", 20)
+
+def cord(pos):
+    global x
+    x = pos[0]//diff
+    global z
+    z = pos[1]//diff
+
+def hightlightbox():
+    for k in range (2):
+        pygame.draw.line(Window, (0, 0, 0), (x * diff-3, (z + k)*diff), (x * diff + diff + 3, (z + k)*diff), 7)
+        pygame.draw.line(Window, (0, 0, 0), ( (x + k)* diff, z * diff, ), ((x + k) * diff, z * diff + diff), 7)
